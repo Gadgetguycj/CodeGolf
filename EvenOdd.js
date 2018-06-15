a823c6e03f204c218723bcb6156d2ea85d62e102
gadgetguycj <clauer@nyit.edu>
2018-06-15 12:27:09 -0400

    Add Link

--- EvenOdd.js
+++ EvenOdd.js
@@ -1,5 +1,8 @@
+// https://codegolf.stackexchange.com/questions/113448/is-this-even-or-odd
 //Even or odd
 let num=3567567565;   //Insert number here
 
+
+num%2===0
 if(num%2===0)console.log("Even");
 else console.log("Odd");
